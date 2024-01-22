238749aa01632ac4d36743adf6e3612b2b7a9987
WRXinYue <3322543587@qq.com>
2024-01-22 13:02:12 +0800

    fix: nav

--- theme/components/SakuraArticle.vue
+++ theme/components/SakuraArticle.vue
@@ -87,7 +87,6 @@ const prevPost = computed(() => posts.value[findCurrentIndex() + 1])
   width: 100%;
   background-size: cover;
   background-position: center center;
-  margin-top: 60px;
   position: relative;
 
   .titlebox {
