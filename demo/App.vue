60a0b0be239f2aad9ccd767799b214e92f3b6318
WRXinYue <3322543587@qq.com>
2024-02-20 00:38:51 +0800

    feat: add hidden example of aplyer lyrics

---
+++ demo/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+/** Hidden ap.lrc */
+function removelrc() {
+  const lrcElement = document.querySelector('.aplayer-icon-lrc')
+  if (lrcElement) {
+    document.removeEventListener('DOMNodeInserted', removelrc)
+    setTimeout(() => {
+      (lrcElement as HTMLElement).click()
+    }, 0)
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('DOMNodeInserted', removelrc)
+})
+</script>
