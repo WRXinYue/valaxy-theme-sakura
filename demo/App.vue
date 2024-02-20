c9af8e46c49723d8f2dc8ba71ee8651f6a01c13e
WRXinYue <3322543587@qq.com>
2024-02-21 01:41:45 +0800

    feat: update aplayer demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,18 +1,58 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue'
 
-/** Hidden ap.lrc */
-function removelrc() {
-  const lrcElement = document.querySelector('.aplayer-icon-lrc')
-  if (lrcElement) {
-    document.removeEventListener('DOMNodeInserted', removelrc)
-    setTimeout(() => {
-      (lrcElement as HTMLElement).click()
-    }, 0)
-  }
+function setupLrcIconHidingObserver() {
+  const observer = new MutationObserver((mutations) => {
+    /** Hidden ap.lrc */
+    function removelrc() {
+      const lrcElement = document.querySelector('.aplayer-lrc .aplayer-lrc-contents .aplayer-lrc-current') as HTMLElement
+      if (lrcElement) {
+        lrcElement.style.display = 'none'
+        const lrcButton = document.querySelector('.aplayer-icon-lrc') as HTMLElement
+        if (lrcElement?.textContent !== 'Loading') {
+          lrcButton.click()
+          lrcElement.style.display = ''
+          observer.disconnect()
+        }
+      }
+    }
+    mutations.forEach((_mutation) => {
+      removelrc()
+    })
+  })
+  const config = { childList: true, subtree: true }
+  observer.observe(document.body, config)
+}
+
+function setupAplayerVisibilityObserver() {
+  const observer = new MutationObserver((mutations) => {
+    function hiddenAplayer() {
+      const aplayerNarrowElement = document.querySelector('.aplayer.aplayer-withlist.aplayer-fixed.aplayer-narrow .aplayer-body') as HTMLElement
+      if (aplayerNarrowElement) {
+        setTimeout(() => {
+          aplayerNarrowElement.style.display = 'initial'
+          requestAnimationFrame(() => {
+            aplayerNarrowElement.style.left = '-66px'
+            observer.disconnect()
+          })
+        }, 0)
+      }
+      else {
+        const aplayerFixedElement = document.querySelector('.aplayer.aplayer-withlist.aplayer-fixed .aplayer-body') as HTMLElement
+        aplayerFixedElement.style.left = '0'
+      }
+    }
+    mutations.forEach((_mutation) => {
+      hiddenAplayer()
+    })
+  })
+
+  const config = { childList: true, subtree: true }
+  observer.observe(document.body, config)
 }
 
 onMounted(() => {
-  document.addEventListener('DOMNodeInserted', removelrc)
+  setupLrcIconHidingObserver()
+  setupAplayerVisibilityObserver()
 })
 </script>
