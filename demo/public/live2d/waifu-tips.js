9866c33a1bd9eb6b65b1d16e213a52378584d61a
WRXinYue <3322543587@qq.com>
2024-02-21 01:45:11 +0800

    feat: update aplayer style demo

--- demo/public/live2d/waifu-tips.js
+++ demo/public/live2d/waifu-tips.js
@@ -58,7 +58,7 @@ function loadWidget(config) {
 	</div>`)
   // https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
   setTimeout(() => {
-    document.getElementById('waifu').style.bottom = 0
+    document.getElementById('waifu').style.bottom = '-10px'
   }, 0);
 
   (function registerEventListener() {
