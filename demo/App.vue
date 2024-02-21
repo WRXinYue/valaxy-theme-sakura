67cf5efae7c6c667e7c9f064831f7763ae734ecc
WRXinYue <3322543587@qq.com>
2024-02-21 18:51:50 +0800

    feat: complete aplyer animation trigger

--- demo/App.vue
+++ demo/App.vue
@@ -25,14 +25,19 @@ function setupLrcIconHidingObserver() {
 }
 
 function setupAplayerVisibilityObserver() {
+  let hasExecuted = false
   const observer = new MutationObserver((mutations) => {
     function hiddenAplayer() {
+      if (hasExecuted)
+        return
       const aplayerNarrowElement = document.querySelector('.aplayer.aplayer-withlist.aplayer-fixed.aplayer-narrow .aplayer-body') as HTMLElement
       if (aplayerNarrowElement) {
+        hasExecuted = true
         setTimeout(() => {
           aplayerNarrowElement.style.display = 'initial'
           requestAnimationFrame(() => {
             aplayerNarrowElement.style.left = '-66px'
+            onMetingLoad()
             observer.disconnect()
           })
         }, 0)
@@ -41,6 +46,7 @@ function setupAplayerVisibilityObserver() {
         const aplayerFixedElement = document.querySelector('.aplayer.aplayer-withlist.aplayer-fixed .aplayer-body') as HTMLElement
         if (aplayerFixedElement)
           aplayerFixedElement.style.left = '0'
+        return false
       }
     }
     mutations.forEach((_mutation) => {
@@ -50,6 +56,34 @@ function setupAplayerVisibilityObserver() {
 
   const config = { childList: true, subtree: true }
   observer.observe(document.body, config)
+}
+
+function onMetingLoad() {
+  setupAplayerVisibility()
+}
+
+function setupAplayerVisibility() {
+  const aplayerFixedElement = document.querySelector('.aplayer.aplayer-withlist.aplayer-fixed .aplayer-body') as HTMLElement
+  const aplayerIconButton = document.querySelector('.aplayer-body .aplayer-miniswitcher .aplayer-icon') as HTMLElement
+
+  let aplayerNarrow = true
+
+  function ToggleAplayerNarrow() {
+    aplayerNarrow = !aplayerNarrow
+  }
+
+  function hiddenAplayer() {
+    if (aplayerNarrow)
+      aplayerFixedElement.style.left = '-66px'
+  }
+
+  function showAplayer() {
+    aplayerFixedElement.style.left = '0'
+  }
+
+  aplayerFixedElement.addEventListener('mouseenter', showAplayer)
+  aplayerFixedElement.addEventListener('mouseleave', hiddenAplayer)
+  aplayerIconButton.addEventListener('click', ToggleAplayerNarrow)
 }
 
 onMounted(() => {
