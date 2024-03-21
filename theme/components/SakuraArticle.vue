f33b4c264dca8a6a26c84d5d91db4d5f2a0ceb85
WRXinYue <3322543587@qq.com>
2024-03-21 20:15:50 +0800

    style: change max width

--- theme/components/SakuraArticle.vue
+++ theme/components/SakuraArticle.vue
@@ -9,7 +9,7 @@ const frontmatter = useFrontmatter()
     <SakuraPageHeader :title="frontmatter.title ?? 'Default Title'" :cover="frontmatter.cover" :author="frontmatter.author" :date="frontmatter.date" />
     <!-- :class="themeConfig.animation && 'element-slide-up'" -->
 
-    <div class="divide-y xl:divide-y-0  divide-gray-200 dark:divide-gray-700 pb-8 max-w-1000px mx-auto">
+    <div class="divide-y xl:divide-y-0  divide-gray-200 dark:divide-gray-700 pb-8 max-w-850px mx-auto">
       <StarterAuthor v-if="frontmatter.author" :frontmatter="frontmatter" />
 
       <div class="article-content divide-y divide-gray-200 dark:divide-gray-700 px-30px">
