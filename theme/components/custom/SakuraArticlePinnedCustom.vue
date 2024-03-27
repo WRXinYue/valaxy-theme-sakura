20c368f8194fea26c1c1527deca1bf1392604b28
WRXinYue <3322543587@qq.com>
2024-03-27 17:11:42 +0800

    feat: add articlePinned judgments

--- theme/components/custom/SakuraArticlePinnedCustom.vue
+++ theme/components/custom/SakuraArticlePinnedCustom.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import { useThemeConfig } from '../../composables'
+
+const themeConfig = useThemeConfig()
+</script>
+
 <template>
-  <div class="<md:hidden">
+  <div v-if="themeConfig.articlePinned" class="<md:hidden">
     <div class="pt-24px px-20px flex items-center text-$st-c-text">
       <div i-fa6-solid:anchor class="mr-1" /> START:DASH!!
     </div>
