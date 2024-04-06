76a83a5191e3e4a8db5fc4cf02ed6653f99246d0
WRXinYue <3322543587@qq.com>
2024-04-06 19:27:33 +0800

    feat: change ocean demo

--- demos/ocean/.valaxy/typed-router.d.ts
+++ demos/ocean/.valaxy/typed-router.d.ts
@@ -27,7 +27,9 @@ declare module 'vue-router/auto-routes' {
     '/categories/': RouteRecordInfo<'/categories/', '/categories', Record<never, never>, Record<never, never>>,
     '/links/': RouteRecordInfo<'/links/', '/links', Record<never, never>, Record<never, never>>,
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
+    '/posts/2024_01_11': RouteRecordInfo<'/posts/2024_01_11', '/posts/2024_01_11', Record<never, never>, Record<never, never>>,
     '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
+    '/posts/Japan': RouteRecordInfo<'/posts/Japan', '/posts/Japan', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
