ef6bb50d828177013957210ffa32c374d0a6126c
WRXinYue <wrxinyue@proton.me>
2024-09-29 20:57:57 +0800

    chore: remove submodules

--- theme/components/SakuraNoticeBoard.vue
+++ theme/components/SakuraNoticeBoard.vue
@@ -11,11 +11,11 @@ const props = withDefaults(defineProps<{
 
 const themeConfig = useThemeConfig()
 
-const noticeMessage = computed(() => props.text || themeConfig.value.notice.message)
+const noticeMessage = computed(() => props.text || themeConfig.value.notice?.message)
 </script>
 
 <template>
-  <div v-if="text" class="mb-5 mt-10 flex items-center border rounded-$st-c-rd border-dashed p-5">
+  <div v-if="noticeMessage" class="mb-5 mt-10 flex items-center border rounded-$st-c-rd border-dashed p-5">
     <div :class="icon" mr-2 />
     <div v-html="noticeMessage" />
   </div>
