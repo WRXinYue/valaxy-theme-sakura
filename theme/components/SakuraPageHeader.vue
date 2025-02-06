0a38ef2ac77db5456969683e9707e3888b2fc138
WRXinYue <wrxinyue@proton.me>
2025-02-06 19:11:45 +0800

    fix: correct page header title style

--- theme/components/SakuraPageHeader.vue
+++ theme/components/SakuraPageHeader.vue
@@ -11,12 +11,8 @@ defineProps<{
     <SakuraImageCard v-if="fm.cover" :src="fm.cover" h="full" w="full" absolute="!" z="-1" />
     <div class="sakura-safe-padding sakura-header-container pb-25px">
       <slot name="title">
-        <div class="sakura-header-title" flex="~ items-center" :class="!fm.cover && 'sakura-braced-text flex-center mx-6%'">
-          <slot name="icon">
-            <div v-if="fm.icon" class="icon" m="r-1" inline-flex align-top :class="fm.icon" />
-          </slot>
-          <span>{{ fm.title }}</span>
-          <span v-if="fm.subTitle"> · {{ fm.subTitle }}</span>
+        <div flex="~" class="sakura-header-title">
+          <SakuraTitle :fm />
         </div>
       </slot>
       <slot />
@@ -32,33 +28,20 @@ defineProps<{
   width: 100%;
   position: relative;
 
-  .sakura-header-title {
-    font-size: 1.8rem;
-    font-weight: 700;
-
-    @include screen('sm') {
-      font-size: 2.3rem;
-    }
-  }
-
   &:not(.has-cover) {
     margin-top: var(--sakura-navbar-spacing);
 
     .sakura-header-title {
-      color: var(--sakura-color-text-deep);
-    }
-  }
-
-  &.has-cover {
-    height: 320px;
-
-    .sakura-header-title {
-      text-shadow: 2px 2px 10px black;
+      justify-content: center;
     }
   }
 
   .sakura-header-container {
     color: white;
+  }
+
+  &.has-cover {
+    height: 320px;
   }
 }
 </style>
---
+++ theme/components/SakuraTitle.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { PostFrontMatter } from 'valaxy'
+
+defineProps<{
+  fm: PostFrontMatter
+}>()
+</script>
+
+<template>
+  <div class="sakura-title" flex="~ items-center" :class="!fm.cover && 'sakura-braced-text flex-center'">
+    <slot name="icon">
+      <div v-if="fm.icon" class="icon" m="r-1" inline-flex align-top :class="fm.icon" />
+    </slot>
+    <span>{{ fm.title }}</span>
+    <span v-if="fm.subTitle"> · {{ fm.subTitle }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use 'valaxy/client/styles/mixins/index.scss' as *;
+
+.sakura-title {
+  font-size: 1.8rem;
+  font-weight: 700;
+  color: var(--sakura-color-text-deep);
+
+  @include screen('sm') {
+    font-size: 2.3rem;
+  }
+}
+</style>
+
+<style lang="scss">
+.has-cover {
+  .sakura-title {
+    color: inherit;
+    text-shadow: 2px 2px 10px black;
+  }
+}
+</style>
