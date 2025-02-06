3ae25989648269f628f70c1a114b1334aaae3423
WRXinYue <wrxinyue@proton.me>
2025-02-07 04:14:47 +0800

    fix: correct the navbar marker style

--- theme/components/SakuraNavbarLink.vue
+++ theme/components/SakuraNavbarLink.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, ref, watch } from 'vue'
+import { nextTick, onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
 import type { NavItem } from '../types/index'
-import { useLayout, useThemeConfig } from '../composables'
 
 const props = withDefaults(defineProps<{
   navbar: NavItem[]
@@ -12,48 +11,45 @@ const props = withDefaults(defineProps<{
   showMarker: false,
 })
 
-const themeConfig = useThemeConfig()
 const route = useRoute()
-const { isIncludes } = useLayout()
 const { locale } = useI18n()
 
 const marker = ref()
-const show = ref(false)
-
-const invert = computed(() => themeConfig.value?.navbarOptions?.invert)
+const preload = ref(true)
+const navLink = ref()
 
 function updateMarker() {
   if (!props.showMarker)
     return
 
   const routeActive = document.querySelector('.sakura-navbar-link .router-link-active') as HTMLElement
-  if (!routeActive)
+
+  const child = routeActive
+  const parent = navLink.value
+
+  if (!routeActive) {
+    console.error('Not found: .sakura-navbar-link .router-link-active')
     return
-
-  marker.value.style.left = `${routeActive.offsetLeft}px`
-  marker.value.style.width = `${routeActive.offsetWidth}px`
-}
-
-function handleShowWithDelay(value: boolean | string | string[]) {
-  // Prevents animation when the navbar is hidden during the first load
-  if (isIncludes(value)) {
-    setTimeout(() => {
-      show.value = true
-    }, 1000)
   }
-  else {
-    show.value = true
-  }
+
+  const childRect = child.getBoundingClientRect()
+  const parentRect = parent.getBoundingClientRect()
+
+  const left = childRect.left - parentRect.left
+
+  marker.value.style.left = `${left}px`
+  marker.value.style.width = `${routeActive?.offsetWidth || 0}px`
 }
 
 watch(() => route.path, () => nextTick(updateMarker))
 watch(() => locale.value, () => setTimeout(() => updateMarker(), 0))
 
 onMounted(() => {
-  if (invert.value)
-    handleShowWithDelay(invert.value)
-  else
-    show.value = true
+  // HACK: Not the most elegant solution, but it works. :)
+  // Consider using a JavaScript alternative if necessary.
+  setTimeout(() => {
+    preload.value = false
+  }, 500)
 
   nextTick(() => {
     updateMarker()
@@ -62,11 +58,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <nav class="sakura-navbar-link">
-    <template v-if="show">
-      <SakuraNavbarLinkItem v-for="(item, i) in navbar" :key="i" v-bind="item" />
-      <div v-if="showMarker" ref="marker" class="marker" />
-    </template>
+  <nav ref="navLink" class="sakura-navbar-link" :class="preload && 'sakura-preload'">
+    <SakuraNavbarLinkItem v-for="(item, i) in navbar" :key="i" v-bind="item" />
+    <div v-if="showMarker" ref="marker" class="marker" />
   </nav>
 </template>
 
