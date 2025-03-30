8e6c0bd876d47f9ac3fd9c09dd21c30f73045060
WRXinYue <wrxinyue@proton.me>
2025-03-30 17:31:41 +0800

    fix: restore missing site information (#19)

--- theme/components/core/ValaxyApp.vue
+++ theme/components/core/ValaxyApp.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+/**
+ * Synchronized with:
+ * https://github.com/YunYouJun/valaxy/blob/main/packages/valaxy/client/components/ValaxyApp.vue
+ */
+
+import { useLayout, useValaxyApp } from 'valaxy'
 // @ts-expect-error virtual module
 import ValaxyUserApp from '/@valaxyjs/UserAppVue'
+
+const layout = useLayout()
+
+if (layout.value !== 'empty') {
+  useValaxyApp()
+}
 </script>
 
 <template>
-  <ValaxyThemeApp />
-  <ValaxyAddons />
-  <ValaxyUserApp />
+  <template v-if="layout !== 'empty'">
+    <ValaxyThemeApp />
+    <ValaxyAddons />
+    <ValaxyUserApp />
+  </template>
 </template>
