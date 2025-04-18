99edc0cf4ea42cef36920207cf855f66a69f465a
WRXinYue <wrxinyue@proton.me>
2025-04-18 13:08:49 +0800

    chore: SakuraWaveRipple scopes classes with a prefix

--- theme/components/SakuraWaveRipple.vue
+++ theme/components/SakuraWaveRipple.vue
@@ -1,12 +1,12 @@
 <template>
   <svg
-    class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28"
+    class="sakura-waves-ripple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28"
     preserveAspectRatio="none" shape-rendering="auto"
   >
     <defs>
       <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
     </defs>
-    <g class="parallax">
+    <g class="sakura-waves-ripple-parallax">
       <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
       <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
       <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
@@ -16,7 +16,7 @@
 </template>
 
 <style lang="scss" scoped>
-.waves {
+.sakura-waves-ripple {
   position: absolute;
   width: 100%;
   height: 15vh;
@@ -24,30 +24,30 @@
   max-height: 150px;
   bottom: 0;
   left: 0;
-}
 
-.parallax > use {
-  animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
-}
+  &-parallax > use {
+    animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+  }
 
-.parallax > use:nth-child(1) {
-  animation-delay: -2s;
-  animation-duration: 7s;
-}
+  &-parallax > use:nth-child(1) {
+    animation-delay: -2s;
+    animation-duration: 7s;
+  }
 
-.parallax > use:nth-child(2) {
-  animation-delay: -3s;
-  animation-duration: 10s;
-}
+  &-parallax > use:nth-child(2) {
+    animation-delay: -3s;
+    animation-duration: 10s;
+  }
 
-.parallax > use:nth-child(3) {
-  animation-delay: -4s;
-  animation-duration: 13s;
-}
+  &-parallax > use:nth-child(3) {
+    animation-delay: -4s;
+    animation-duration: 13s;
+  }
 
-.parallax > use:nth-child(4) {
-  animation-delay: -5s;
-  animation-duration: 20s;
+  &-parallax > use:nth-child(4) {
+    animation-delay: -5s;
+    animation-duration: 20s;
+  }
 }
 
 @keyframes move-forever {
