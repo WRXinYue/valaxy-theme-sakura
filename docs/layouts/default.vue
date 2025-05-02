7bb95fb97722a658a6f496b31918bf0453e12deb
WRXinYue <wrxinyue@proton.me>
2025-05-02 19:32:39 +0800

    feat(docs): add git log contributor

---
+++ docs/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <OceanusArticle>
+      <RouterView v-slot="{ Component }">
+        <component :is="Component">
+          <template #main-nav-before>
+            <GitLogContributor />
+          </template>
+          <template #main-nav>
+            <OceanusArticleNav />
+          </template>
+        </component>
+      </RouterView>
+    </OceanusArticle>
+  </Layout>
+</template>
