38bcfcd3cc7a99b91ae2bb2a9ad18e24cda44158
WRXinYue <wrxinyue@proton.me>
2025-05-02 19:41:59 +0800

    chore(deps): upgrade valaxy to v0.23.3

--- theme/node/plugins.ts
+++ theme/node/plugins.ts
@@ -4,6 +4,7 @@ import type { ThemeConfig } from '../types'
 export function themePlugin(themeConfig: ThemeConfig): PluginOption {
   return {
     name: 'valaxy-theme-sakura',
+    enforce: 'pre',
 
     config() {
       const primaryColor = themeConfig.ui.primary
